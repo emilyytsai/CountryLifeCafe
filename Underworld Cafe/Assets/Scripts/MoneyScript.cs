914f6a95c90a623419f6cd546cac9a007bc7939f
dianella ˚ʚ♡ɞ˚ <143135166+dianellasy@users.noreply.github.com>
2025-02-20 11:38:36 -0800

    Created Different Money Variables & Functions that Add to Total/Initial

--- Underworld Cafe/Assets/Scripts/MoneyScript.cs
+++ Underworld Cafe/Assets/Scripts/MoneyScript.cs
@@ -6,11 +6,36 @@ using TMPro;
 public class MoneyScript : MonoBehaviour
 {
     [SerializeField]
-    public float Money = 0f;
     public TextMeshProUGUI moneyText;
+    public float initial_and_total_money = 0f;
+    public float five_money = 5f;
+    public float ten_money = 10f;
+    public float fifteen_money = 15f;
+
+    bool customer_served = false;
 
     public void Start() 
     {
-        moneyText.text = "$ " + Money.ToString();
+        moneyText.text = "$ " + initial_and_total_money.ToString();
+        // customer_served = false;
+    }
+
+    public void AddFiveTokens() 
+    {
+        initial_and_total_money = initial_and_total_money + five_money;
+        moneyText.text = "$ " + initial_and_total_money.ToString();
+        customer_served = true;
+    }
+
+    public void AddTenTokens() 
+    {
+        initial_and_total_money = initial_and_total_money + ten_money;
+        moneyText.text = "$ " + initial_and_total_money.ToString();
+    }
+
+    public void AddFifteenTokens() 
+    {
+        initial_and_total_money = initial_and_total_money + fifteen_money;
+        moneyText.text = "$ " + initial_and_total_money.ToString();
     }
 }
