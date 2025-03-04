34eaa990f3b4f85f9bddbfc63c569add73ae0f12
emilyytsai <emilytsai@csu.fullerton.edu>
2025-03-04 08:18:57 -0800

    selecting an object tagged as 'interactable' will now be highlighted

--- Underworld Cafe/Assets/Scripts/InputHandler.cs
+++ Underworld Cafe/Assets/Scripts/InputHandler.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+
 public class InputHandler : MonoBehaviour
 {
-   private Camera _mainCamera;
+    private Camera _mainCamera;
+
+    private GameObject last_selected;
+    private Color initial_color;
 
     private void Awake()
     {
@@ -16,7 +20,49 @@ public class InputHandler : MonoBehaviour
         var rayHit = Physics2D.GetRayIntersection(_mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue()));
         if (!rayHit.collider) return;
 
-        Debug.Log(rayHit.collider.gameObject.name);
+        GameObject selected_object = rayHit.collider.gameObject;
+        Debug.Log(selected_object.name);
 
+        if (last_selected == selected_object)
+        {
+            reset();
+            last_selected = null; //deselecting the object
+            return;
+        }
+
+        if (last_selected != null)
+        {
+            reset();
+        }
+
+        highlight(selected_object); //hihglight the last object that was clicked on
+
+        last_selected = selected_object;
+    }
+
+    private void highlight(GameObject selected_object)
+    {
+        SpriteRenderer renderer = selected_object.GetComponent<SpriteRenderer>();
+
+        if (renderer != null)
+        {
+            //store the initial color (which should be nothing bc not highlighted yet)
+            initial_color = renderer.color;
+
+            //switch the color to white
+            renderer.color = new Color(1f, 1f, 1f, 0.15f); //R,G,B,alpha 
+        }
+    }
+
+    private void reset()
+    {
+        if (last_selected != null)
+        {
+            SpriteRenderer renderer = last_selected.GetComponent<SpriteRenderer>();
+            if (renderer != null)
+            {
+                renderer.color = initial_color; //reset to the initial color (remove highlight)
+            }
+        }
     }
 }
