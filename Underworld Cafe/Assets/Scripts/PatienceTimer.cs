7c74e294ce23bcc80c6565a6fd4afd936a152df4
Kelsey <ks.km.tg@gmail.com>
2025-03-06 12:41:59 -0800

    got patience bar working yippee

--- Underworld Cafe/Assets/Scripts/PatienceTimer.cs
+++ Underworld Cafe/Assets/Scripts/PatienceTimer.cs
@@ -17,50 +17,69 @@ public class PatienceTimer : MonoBehaviour
 {
     //public TextMeshProUGUI healthText;
     //public Image healthRing;
-    
+    public Image patienceBar;
+
     //public PlayerScript player;
+    public Customer customerScript; //reference customer script variables
 
     //private float lerpSpeed;
-
-
-    //private void Start()
+    private float lerpSpeed;
+ //private void Start()
     //{
+    private void Start()
+    {
         ////find player in the scene
         //player = FindObjectOfType<PlayerScript>();
     //}
-
-
-    //private void Update()
+       // customer = FindObjectOfType<customer>();
+    }
+  //private void Update()
     //{
+    private void Update()
+    {
         ////speed that the health indicator gets filled
         //lerpSpeed = 3f * Time.deltaTime;
+        lerpSpeed += Time.deltaTime;
 
         //healthText.text = " " + player.PlayerHealth.ToString();
 
         //HealthFiller(); //call here to also update the filler
         //ColorChanger(); //updates color
     //}
+        BarFiller(); //call here to also update the filler
+        ColorChanger(); //updates color
+    }
 
 
     //void HealthFiller()
     //{
+    void BarFiller()
+    {
         ////dividing health by max health since max fill amount is 1 so now value of health = between 0 and 1
         //////also convert the int to float for the division
         //float currentHealth = (float)player.PlayerHealth / player.MaxPlayerHealth;
+        //float current_patience = (float)customer.customer_patience / customer.max_patience;
 
         ////reference: https://docs.unity3d.com/ScriptReference/Mathf.Lerp.html
         //////https://gamedevbeginner.com/the-right-way-to-lerp-in-unity-with-examples/
         //healthRing.fillAmount = Mathf.Lerp(healthRing.fillAmount, currentHealth, lerpSpeed);
     //}
+        patienceBar.fillAmount -= 1.0f / 30f * Time.deltaTime;
+    }
 
 
     //void ColorChanger()
     //{
         //float currentHealth = (float)player.PlayerHealth / player.MaxPlayerHealth;
+    void ColorChanger()
+    {
+        //float current_patience = (float)customer.customer_patience / customer.max_patience;
 
         ////https://docs.unity3d.com/ScriptReference/Color.Lerp.html
         //Color healthColor = Color.Lerp(Color.red, Color.green, currentHealth);
         //healthRing.color = healthColor;
     //}
-    
+        Color bar_color = Color.Lerp(Color.green, Color.red, lerpSpeed/30f);
+        patienceBar.color = bar_color;
+    }
 }
