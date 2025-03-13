eb1c99774e85f68dbcb648ab16237835825d3012
dianella ˚ʚ♡ɞ˚ <143135166+dianellasy@users.noreply.github.com>
2025-03-12 22:40:33 -0700

    Implemented Click Sound Effect for Each Button

--- Underworld Cafe/Assets/Scripts/ChangeScene.cs
+++ Underworld Cafe/Assets/Scripts/ChangeScene.cs
@@ -5,12 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
     [SerializeField]
-    private string Name;
-    //for mouse click sound effect
+    private string Name; // Name of the scene to load
     private AudioManager audio_manager;
-    
+
     private void Start()
     {
         audio_manager = FindAnyObjectByType<AudioManager>();
@@ -18,12 +16,18 @@ public class MainMenu : MonoBehaviour
 
     public void ChangeScene()
     {
-        SceneManager.LoadScene(Name); // will load the game/change scenes when button is pressed
+        StartCoroutine(WaitAndChangeScene()); // Start the coroutine to wait before changing the scene
     }
 
     public void click_sound()
     {
-        audio_manager.Play("Button Click"); //play this sfx when a menu UI button is clicked
+        audio_manager.Play("Button Click"); // Play the button click sound effect
     }
 
+    public IEnumerator WaitAndChangeScene()
+    {
+        click_sound(); // Play the sound effect
+        yield return new WaitForSeconds(0.3f); // Wait for 0.3 seconds
+        SceneManager.LoadScene(Name); // Load the new scene after the delay
+    }
 }
