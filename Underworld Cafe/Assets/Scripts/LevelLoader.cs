c9c7e8e8c8adbd8c985bb3efdbcf384cbad3abc4
dianella ˚ʚ♡ɞ˚ <143135166+dianellasy@users.noreply.github.com>
2025-04-05 18:12:44 -0700

    Delete Commented Code in LevelLoader Script

--- Underworld Cafe/Assets/Scripts/LevelLoader.cs
+++ Underworld Cafe/Assets/Scripts/LevelLoader.cs
@@ -8,15 +8,6 @@ public class LevelLoader : MonoBehaviour
     public Animator transition;
     public float transitionTime = 1f;
 
-    // Update is called once per frame
-    // void Update()
-    // {
-    //     // if(Input.GetMouseButtonDown(0))
-    //     // {
-    //     //     LoadNextLevel();
-    //     // }
-    // }
-
     public void LoadNextLevel() {
         StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
     }
